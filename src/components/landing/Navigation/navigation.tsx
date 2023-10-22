92f7f8f1c8c76e70a9eccc027bc032fb1cda8ca0
Wojciech Marek <marek.wojciech@outlook.com>
2023-10-22 17:57:32 +0200

    feat: add mobile menu

--- src/components/landing/Navigation/navigation.tsx
+++ src/components/landing/Navigation/navigation.tsx
@@ -1,53 +1,90 @@
 "use client";
 
-import { Menu } from "lucide-react";
+import { Menu, X } from "lucide-react";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 type Props = {};
 
 export const Navigation = (props: Props) => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const handleMobileMenuClick = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
-    <nav className="flex items-center justify-between py-8 max-w-5xl mx-auto lg:mx-auto px-8 md:px-4">
-      <div className="flex items-center gap-4">
-        {/* <Image src="/logo.svg" width={40} height={40} /> */}
-        <h1 className="text-2xl font-bold">
-          <Link
-            className="bg-gradient-to-r from-[#B24A6D] to-[#7E78A6] bg-clip-text text-transparent"
-            href="/"
-          >
-            Pocket Bank
-          </Link>
-        </h1>
+    <nav>
+      <div className="flex items-center justify-between py-8 max-w-5xl mx-auto lg:mx-auto px-8 md:px-4">
+        <div className="flex items-center gap-4">
+          {/* <Image src="/logo.svg" width={40} height={40} /> */}
+          <h1 className="text-2xl font-bold">
+            <Link
+              className="bg-gradient-to-r from-[#B24A6D] to-[#7E78A6] bg-clip-text text-transparent"
+              href="/"
+            >
+              Pocket Bank
+            </Link>
+          </h1>
+        </div>
+        <ul className="gap-8 hidden md:flex">
+          <li>
+            <Link href="/features">Features</Link>
+          </li>
+          <li>
+            <Link href="/plans">Plans</Link>
+          </li>
+          <li>
+            <Link href="/community">Community</Link>
+          </li>
+          <li>
+            <Link href="/help">Help</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link
+              className="px-6 py-3 rounded-full bg-gradient-to-r from-[#B24A6D] to-[#564ED7] font-bold"
+              href="/login"
+            >
+              Get Started
+            </Link>
+          </li>
+        </ul>
+        <button className="md:hidden p-1" onClick={handleMobileMenuClick}>
+          {isMobileMenuOpen ? <X /> : <Menu />}
+        </button>
       </div>
-      <ul className="gap-8 hidden md:flex">
-        <li>
-          <Link href="/features">Features</Link>
-        </li>
-        <li>
-          <Link href="/plans">Plans</Link>
-        </li>
-        <li>
-          <Link href="/community">Community</Link>
-        </li>
-        <li>
-          <Link href="/help">Help</Link>
-        </li>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link
-            className="px-6 py-3 rounded-full bg-gradient-to-r from-[#B24A6D] to-[#564ED7] font-bold"
-            href="/login"
-          >
-            Get Started
-          </Link>
-        </li>
-      </ul>
-      <button className="md:hidden p-1">
-        <Menu />
-      </button>
+      {isMobileMenuOpen && (
+        <div className="absolute h-fit py-6 w-screen bg-[#0f0f0f]">
+          <ul className="gap-4 flex flex-col items-center text-xl">
+            <li>
+              <Link href="/features">Features</Link>
+            </li>
+            <li>
+              <Link href="/plans">Plans</Link>
+            </li>
+            <li>
+              <Link href="/community">Community</Link>
+            </li>
+            <li>
+              <Link href="/help">Help</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+            <li className="my-2">
+              <Link
+                className="px-6 py-3 rounded-full bg-gradient-to-r from-[#B24A6D] to-[#564ED7] font-bold"
+                href="/login"
+              >
+                Get Started
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
     </nav>
   );
 };
